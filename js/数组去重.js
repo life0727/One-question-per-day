53fad6311a60a64b907ec476d96fc156afa905fb
life0727 <mmlife0727@gmail.com>
2019-03-13 10:49:46 +0800

    '数组去重'

---
+++ js/数组去重.js
@@ -0,0 +1,18 @@
+const arr = ['1',1,22,'22','hello word',233333,6666,6666,6666];
+//filter方法
+// const res = arr.filter((item,index,self) =>{
+//    return index == self.indexOf(item);
+// })
+
+//set方法 类似于数组，但是成员的值都是唯一的，没有重复的值。 [...new Set(array)]
+//const res = [...new Set(arr)]
+ 
+//循环遍历
+const res = []; 
+for(let i of arr){
+    res.indexOf(i) == -1 ? res.push(i) : ''
+}
+
+
+console.log(res)
+
