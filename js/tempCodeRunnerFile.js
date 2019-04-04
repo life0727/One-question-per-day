87f54e814cf36fd249e1cc8dd71b9284313dd3ca
life0727 <mmlife0727@gmail.com>
2019-04-04 16:35:14 +0800

    跨域

--- js/tempCodeRunnerFile.js
+++ js/tempCodeRunnerFile.js
@@ -1,1 +1,11 @@
-            //console.log(res)
+      
+      function getNum() {
+          var num = 1
+          while(num){
+              if(num % 2 == 1 && num % 3 == 0 && num % 4 == 1 && num % 5 == 4 && num % 6 == 3 && num % 7 == 0 && num % 8 == 1 && num % 9 == 0){
+                return num 
+              }
+              num ++
+          }
+      } 
+     console.log(getNum())
