fdabf318eb46e204f703c6b9fdd3caa87afe26ad
life0727 <mmlife0727@gmail.com>
2019-04-09 15:24:19 +0800

    ajax概念

--- js/tempCodeRunnerFile.js
+++ js/tempCodeRunnerFile.js
@@ -1,11 +1,34 @@
+// function maxQueter(arr){
+//   let _arr = [...arr];
+//   let que;
+//   let resArr = []
+//   arr.forEach((item,index,self) => {
+//     if(item >= Math.max(..._arr.slice(index,arr.length))){
+//       que = [...self.slice(index,self.length)]
+//       resArr.push(que)
+//       return;
+//     }else if(item < Math.max(..._arr.slice(index,arr.length))){
       
-      function getNum() {
-          var num = 1
-          while(num){
-              if(num % 2 == 1 && num % 3 == 0 && num % 4 == 1 && num % 5 == 4 && num % 6 == 3 && num % 7 == 0 && num % 8 == 1 && num % 9 == 0){
-                return num 
-              }
-              num ++
-          }
-      } 
-     console.log(getNum())
+//       if(self.lastIndexOf(item) !== index){
+//         console.log(item)
+//         que = [..._arr.slice(index,self.lastIndexOf(item))]
+//         resArr.push(que)
+//         arr.splice(index,self.lastIndexOf(item) - index)
+//       }else{
+//         que = [item]
+//         resArr.push(que)
+//         arr.splice(index,1)
+//       }
+//     }
+//   });
+//   console.log(resArr)
+//   return '最大块数：' + resArr.length
+// }
+// //maxQueter([1,8,2,3,6,2,7])
+// console.log(maxQueter([1,2,3,4,1,2]))
+
+function define(url,timeOut = 200,option) {
+    console.log(timeOut)
+}
+
+define()
