251d257b6fe7566993d0261522d1c1c0e072ad9c
fuquanmeng <mmlife0727@gmail.com>
2019-07-24 13:02:10 +0800

    flat

--- js/数组扁平化.js
+++ js/数组扁平化.js
@@ -1,10 +1,11 @@
+
 var arr = [1, [2, [3, 4]]];
 
 //数组的flat函数
 //console.log(arr.flat(n)) //n 表示扁平数组深度 1,2,3,Infinity 注：兼容较差 edge，ie不兼容
 
 //自己写flater函数 递归
-function flater(arg){
+/* function flater(arg){
     var res = [];
     for(let i of arg){
         if(Array.isArray(i)){
@@ -17,4 +18,10 @@ function flater(arg){
     return res;
 }
 
-console.log(flater(arr))
+console.log(flater(arr)) */
+
+
+Array.prototype._flat = function(){
+    return [].concat(...this.map(item => Array.isArray(item) ? item._flat() : [item]))
+}
+console.log(arr._flat())
