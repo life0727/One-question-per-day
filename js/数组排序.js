9c8b2723f6bc376a71babb30e9fd536ed43360dc
liiffe <mmlife0727@gmail.com>
2019-12-22 17:56:01 +0800

    Update 数组排序.js

--- js/数组排序.js
+++ js/数组排序.js
@@ -17,12 +17,7 @@ var arr = [4,2,3,8,9];
 function bubble (arr){
     for(let i = 0;i < arr.length - 1; i++){
         for(let j = 0; j < arr.length - i - 1; j++){
-            console.log(arr[j + 1])
-            if(arr[j] > arr[j + 1]){
-                let temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp
-            }
+           arr[j] > arr[j + 1] && ([arr[j],arr[j + 1]] = [arr[j + 1],arr[j]])
         }
     }
     return arr
