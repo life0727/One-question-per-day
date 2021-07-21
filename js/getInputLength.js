6367d81167bd8321329135048d531a653a0717b6
liiffe <mmlife0727@gmail.com>
2021-07-21 18:18:28 +0800

    Create getInputLength.js

---
+++ js/getInputLength.js
@@ -0,0 +1,13 @@
+export function getInputLength(val) {
+    let len = 0;
+    for (let i = 0; i < val.length; i++) {
+        let a = val.charAt(i);
+        if (a.match(/[^\x00-\xff]/ig) != null) {
+            len += 2;
+        }
+        else {
+            len += 1;
+        }
+    }
+    return len;
+}
